be60a279f99683b710e6ca9f075655fef20e81f3
Riyad Hamdan <riyad99662@gmail.com>
2026-06-04 23:20:43 +0300

    Fix firebase crash on production

--- users/firebase_service.py
+++ users/firebase_service.py
@@ -1,21 +1,26 @@
 from django.conf import settings
 import firebase_admin
+from firebase_admin import credentials, messaging
+import os
 
-from firebase_admin import credentials
-from firebase_admin import messaging
+firebase_app = None
 
-cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
+if hasattr(settings, "FIREBASE_CREDENTIALS_PATH") and os.path.exists(
+    settings.FIREBASE_CREDENTIALS_PATH
+):
 
+    cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
 
-firebase_admin.initialize_app(cred)
-
+    firebase_app = firebase_admin.initialize_app(cred)
 
 def send_push_notification(
     user,
     title,
     body,
 ):
-
+    if firebase_app is None :
+        return 
+    
     tokens = user.device_tokens.all()
 
     for device in tokens:
