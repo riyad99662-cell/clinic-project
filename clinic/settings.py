4b31f704746552e80e0ad82f08d7e8b2760e31e5
Riyad Hamdan <riyad99662@gmail.com>
2026-06-05 13:58:00 +0300

    add Enviroment variablse

--- clinic/settings.py
+++ clinic/settings.py
@@ -178,8 +178,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "riyad99662@gmail.com"
-EMAIL_HOST_PASSWORD = "znzscerlygusgglc"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 
 MEDIA_URL = "/media/"
