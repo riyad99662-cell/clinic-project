683362259284f79e94ca65e20b81ae2f3ab3ae48
Riyad Hamdan <riyad99662@gmail.com>
2026-06-11 08:09:37 +0300

    Update JWT token lifetime

--- clinic/settings.py
+++ clinic/settings.py
@@ -162,9 +162,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
 }
 
+
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),      # مدة حياة الـ Access Token
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),    # مدة حياة الـ Refresh Token
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
+
 
 # من اجل ال AI Analysis
 from dotenv import load_dotenv
